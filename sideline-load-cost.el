2821880d73226820e2c9f7fbcfc34dce5a6e9ae6
Jen-Chieh Shen <jcs090218@gmail.com>
2024-11-30 19:19:22 +0800

    feat: Drop Emacs 27.x support

--- sideline-load-cost.el
+++ sideline-load-cost.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/emacs-sideline/sideline-load-cost
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "27.1") (sideline "0.1.0"))
+;; Package-Requires: ((emacs "28.1") (sideline "0.1.0"))
 ;; Keywords: help
 
 ;; This file is not part of GNU Emacs.
